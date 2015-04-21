7656711b3a563b5d817d58dda49d8b84798dcff3
Glenn Qian <qianjy1990@gmail.com>
2015-04-21 09:57:11 -0400

    minor modification

--- DealingWithWeatherData/download_csv.py
+++ DealingWithWeatherData/download_csv.py
@@ -1,13 +1,14 @@
 __author__ = 'chianti'
 
 import requests
-import numpy as np
 
-# Change the month manually, download the data for all 12 months
-for i in np.arange(1, 32):
-    r = requests.get('http://www.wunderground.com/history/airport/KNYC/2013/12/'+str(i)+'/DailyHistory.html?req_city=New+York&req_state=NY&req_statename=&reqdb.zip=10106&reqdb.magic=4&reqdb.wmo=99999&format=1')
-    data = r.text
-    f = open('201312'+str(i).zfill(2)+'.csv', 'a')
-    f.write(data)
-    f.close()
+url = 'http://www.wunderground.com/history/airport/KNYC/2013/{}/{}/DailyHistory.html?req_city=New+York&req_state=NY&req_statename=&reqdb.zip=10106&reqdb.magic=4&reqdb.wmo=99999&format=1'
 
+# Change the month manually, download the data for all 12 month
+for j in range(1, 13):
+    for i in range(1, 32):
+        r = requests.get(url.format(j, i))
+        data = r.text
+        f = open('201312'+str(i).zfill(2)+'.csv', 'a')
+        f.write(data)
+        f.close()
